3b41c30d95dbda16fbf8eaa771839db55e2b99eb
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-11-28 20:17:02 +0100

    setup fonts, emoji and color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import JoyPixels from '../../node_modules/emoji-toolkit/lib/js/joypixels.min.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'da-bubble';
+  title = 'DABubble';
 }
