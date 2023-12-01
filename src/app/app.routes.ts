df53b0f70de947c60f902b2c56bbeb00f21e90d6
Marcel Schliwka <marcelschliwka@protonmail.com>
2023-12-01 15:59:40 +0100

    added github ci workflow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,22 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationContentComponent } from './login/registration-content/registration-content.component';
 import { LoginContentComponent } from './login/login-content/login-content.component';
 import { ForgetPasswordContentComponent } from './login/forget-password-content/forget-password-content.component';
+import { IntroScreenComponent } from './intro-screen/intro-screen.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginContentComponent },
-  { path: 'login', component: LoginContentComponent },
-  { path: 'register', component: RegistrationContentComponent },
-  { path: 'reset', component: ForgetPasswordContentComponent },
+  {
+    path: '',
+    component: IntroScreenComponent,
+    children: [
+      { path: '', component: LoginContentComponent },
+      { path: 'login', component: LoginContentComponent },
+      { path: 'register', component: RegistrationContentComponent },
+      { path: 'reset', component: ForgetPasswordContentComponent },
+    ],
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
 ];
