138e337f92c171be3f8b599b3808bab351df7a6b
Marcel Schliwka <marcelschliwka@protonmail.com>
2024-06-02 02:49:46 +0200

    Deleted old firebaseconfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"***REMOVED***","appId":"***REMOVED***","storageBucket":"***REMOVED***.appspot.com","apiKey":"***REMOVED***","authDomain":"***REMOVED***.firebaseapp.com","messagingSenderId":"***REMOVED***"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase()))]
+  providers: [provideRouter(routes)],
 };
